4cf654612e70ded103a2e6897c0882a559afe6e0
= <=>
2024-09-02 05:59:19 +0100

    added

--- src/config.js
+++ src/config.js
@@ -1,5 +1,5 @@
 export const allconfig = {
-    env: 'dev',
+    env: 'prod',
     dev_api_url : 'http://172.20.10.10:8000',
     dev_site_url: 'http://localhost:3000',
     prod_api_url : 'https://studentshare-server.vercel.app',
